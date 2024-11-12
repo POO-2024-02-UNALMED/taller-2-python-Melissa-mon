21c6beab3ad51c00d7323e11e3a26d7b0fec2a43
Melissa-mon <71413547+Melissa-mon@users.noreply.github.com>
2024-11-12 02:19:46 +0000

    Taller

--- main.py
+++ main.py
@@ -5,9 +5,36 @@ class Asiento:
         self.precio = precio
 
     def cambiarColor(self, nuevoColor):
-        if nuevoColor in ["rojo", "verde", "amarillo", "negro", "blanco"]:
+        if nuevoColor == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
             self.color = nuevoColor
+            
+# Clase Auto
+class Auto:
+    cantidadCreados = 0
 
+    def __init__(self, modelo, precio, asiento, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asiento
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    
+    def cantidadAsientos(self):
+        cantidadCreados = 0 
+        for elementro in self.asientos: 
+            if type(elementro)== Asiento: 
+                cantidad += 1
+        return (cantidad)
+    
+    def verificarIntegridad(self):
+         if self.registro == self.motor.registro:
+             for elemento in self.asientos:
+                    if elementro.registro != self.registro:
+                      return("Las piezas no son originales")
+                    else:
+                        return("Auto original")
+         return("Las piezas no son originales")
 class Motor:
     def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros = numeroCilindros
@@ -20,15 +47,3 @@ class Motor:
     def asignarTipo(self, nuevoTipo):
         if nuevoTipo in ["electrico", "gasolina"]:
             self.tipo = nuevoTipo
-            
-# Clase Auto
-class Auto:
-    cantidadCreados = 0
-
-    def __init__(self, modelo, precio, marca, motor, registro):
-        self.modelo = modelo
-        self.precio = precio
-        self.asientos = []
-        self.marca = marca
-        self.motor
-        self.registro = registro
