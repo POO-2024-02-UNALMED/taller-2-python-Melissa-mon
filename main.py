46d03be5fdd1322e073c6a619e956f269e5ab369
Melissa-mon <71413547+Melissa-mon@users.noreply.github.com>
2024-11-12 01:41:28 +0000

    Taller

--- main.py
+++ main.py
@@ -5,28 +5,22 @@ class Asiento:
         self.registro = registro
 
     def cambiarColor(self, nuevoColor):
-        # Validación de colores permitidos
         if nuevoColor in ["rojo", "verde", "amarillo", "negro", "blanco"]:
             self.color = nuevoColor
-
 
 # Clase Motor
 class Motor:
     def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros = numeroCilindros
-        self.tipo = tipo if tipo in ["electrico", "gasolina"] else None
+        self.tipo = tipo
         self.registro = registro
 
     def cambiarRegistro(self, nuevoRegistro):
-        # Aseguramos que el nuevo registro sea un entero
-        if isinstance(nuevoRegistro, int):
-            self.registro = nuevoRegistro
+        self.registro = nuevoRegistro
 
     def asignarTipo(self, nuevoTipo):
-        # Validación de tipos de motor permitidos
         if nuevoTipo in ["electrico", "gasolina"]:
             self.tipo = nuevoTipo
-
 
 # Clase Auto
 class Auto:
@@ -35,21 +29,19 @@ class Auto:
     def __init__(self, modelo, precio, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
-        self.asientos = []  # Lista de objetos Asiento
+        self.asientos = []
         self.marca = marca
         self.motor = motor
         self.registro = registro
         Auto.cantidadCreados += 1
 
     def cantidadAsientos(self):
-        # Retorna la cantidad de objetos Asiento en la lista asientos
-        return len([asiento for asiento in self.asientos if isinstance(asiento, Asiento)])
+        return sum(1 for asiento in self.asientos if isinstance(asiento, Asiento))
 
     def verificarIntegridad(self):
-        # Verificación de la integridad de los registros
         if self.registro != self.motor.registro:
             return "Las piezas no son originales"
         for asiento in self.asientos:
-            if asiento.registro != self.registro:
+            if asiento and asiento.registro != self.registro:
                 return "Las piezas no son originales"
         return "Auto original"
